533992b7c37ab25980f677b7087032de68145119
dogeMcdogeface <username@domain.example>
2022-11-12 19:04:07 +0100

    Added ajax behaviour

--- var/www/editor/script.js
+++ var/www/editor/script.js
@@ -2,11 +2,22 @@
 const outputText = document.getElementById('outputText');
 
 function SubmitRun() {
-  PrintToConsole("Running Program");
+    PrintToConsole("Running Program");
+    loadDoc();
 }
 
 function PrintToConsole(txt, color){
-outputText.innerHTML += "\n" + txt;
-outputText.scrollTop = outputText.scrollHeight;
-//TODO: aggiungere colori alle linee stampate (vedi trace di swi prolog)
+    outputText.innerHTML += "\n" + txt;
+    outputText.scrollTop = outputText.scrollHeight;
+    //TODO: aggiungere colori alle linee stampate (vedi trace di swi prolog) (vedi /docs/todo5.png)
+}
+
+
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    PrintToConsole(this.responseText);
+  }
+  xhttp.open("POST", "/api/test");
+  xhttp.send();
 }
