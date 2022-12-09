8449a20f67e055b162d479969030286541f570ab
dogeMcdogeface <username@domain.example>
2022-12-09 17:54:53 +0100

    Aggiunta icona nella tray

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -3,6 +3,7 @@ package Main;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import Server.MyServer;
 
@@ -10,11 +11,17 @@ import Server.MyServer;
 public class Main {
     public static void main(String[] args) throws Exception {
         MyServer.init(3636);
-
-
-        //openDefaultBrowser(new URI("http://localhost:3636/editor/index.html"));
+        MyTrayIcon.init();
     }
 
+
+    public static void openWebGUI(){
+        try {
+            openDefaultBrowser(new URI("http://localhost:3636/editor/index.html"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void openDefaultBrowser(URI uri) throws IOException {
         if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
