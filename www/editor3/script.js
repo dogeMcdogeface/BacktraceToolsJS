fcd573aacc5bc085fa05c5e4ad82daccc2dce1c1
dogeMcdogeface <username@domain.example>
2023-02-17 20:02:17 +0100

    Improved console area

--- www/editor3/script.js
+++ www/editor3/script.js
@@ -1,29 +1,11 @@
 const consoleArea = document.querySelector('console-area');
-document.querySelectorAll('button').forEach(button => button.addEventListener('click', event => {
-//console.log(event.target.id);
-switch (event.target.id) {
-  case 'answer-request-button':
-    answerRequest();
-    break;
-  case 'clear-console-button':
-    clearConsole();
-    break;
-  case 'show-tree-button':
-    showTree();
-    break;
-  case 'header-New-button':
-    headerNew();
-    break;
-  case 'header-Open-button':
-    headerOpen();
-    break;
-  case 'header-Save-button':
-    headerSave();
-    break;
-  default:
-  console.log("Button with undefined behaviour, id="+event.target.id);
+
+function main(){
+    console.log("Starting Backtrace Tools");
+    testConsole();
 }
-}));
+
+//Functions called by buttons in the interface
 
 function answerRequest() {
   console.log("answerRequest");
@@ -53,13 +35,47 @@ function headerSave() {
 
 
 
-//Print random error messages to the console, as an example
-const htmlColors = ['red', 'blue', 'green', 'orange', 'purple', 'pink', 'brown', 'gray', 'black'];
-const errorMessages = ['Fatal Exception', 'Undefined Variable', 'Syntax Error', 'Index Out of Bounds', 'Memory Overflow'];
-for (let i = 0; i < 15; i++) {
-  const color = htmlColors[Math.floor(Math.random() * htmlColors.length)];
-  const message = `${errorMessages[Math.floor(Math.random() * errorMessages.length)]}: ${Math.random().toString(36).substring(2, 15)}`;
-  consoleArea.write(color, message);
+
+
+
+
+
+document.querySelectorAll('button').forEach(button => button.addEventListener('click', event => {
+switch (event.target.id) {
+  case 'answer-request-button':
+    answerRequest();
+    break;
+  case 'clear-console-button':
+    clearConsole();
+    break;
+  case 'show-tree-button':
+    showTree();
+    break;
+  case 'header-New-button':
+    headerNew();
+    break;
+  case 'header-Open-button':
+    headerOpen();
+    break;
+  case 'header-Save-button':
+    headerSave();
+    break;
+  default:
+  console.log("Button with undefined behaviour, id="+event.target.id);
 }
-const t=document.createElement`table`,s=t.style;s.border='1px solid black';for(let i=2;i--;)for(let r=t.insertRow(),j=3;j--;)r.insertCell().style.cssText='border:1px solid black;padding:10px',r.cells[2-j].textContent=`Row ${i+1}, Column ${j+1}`;
-consoleArea.insert(t);
+}));
+main();
+
+
+    //Print random error messages to the console, as an example
+function testConsole(){
+    const htmlColors = ['red', 'blue', 'green', 'orange', 'purple', 'pink', 'brown', 'gray', 'black'];
+    const errorMessages = ['Fatal Exception', 'Undefined Variable', 'Syntax Error', 'Index Out of Bounds', 'Memory Overflow'];
+    for (let i = 0; i < 15; i++) {
+      const color = htmlColors[Math.floor(Math.random() * htmlColors.length)];
+      const message = `${errorMessages[Math.floor(Math.random() * errorMessages.length)]}: ${Math.random().toString(36).substring(2, 15)}`;
+      consoleArea.write(color, message);
+    }
+    const t=document.createElement`table`,s=t.style;s.border='1px solid black';for(let i=2;i--;)for(let r=t.insertRow(),j=3;j--;)r.insertCell().style.cssText='border:1px solid black;padding:10px',r.cells[2-j].textContent=`Row ${i+1}, Column ${j+1}`;
+    consoleArea.insert(t);
+}
