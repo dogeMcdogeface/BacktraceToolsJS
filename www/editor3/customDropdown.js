daed404a3056fc793885055915b014cb82ca4d15
dogeMcdogeface <username@domain.example>
2023-02-17 19:03:35 +0100

    Aggiunto header con menu

--- www/editor3/customDropdown.js
+++ www/editor3/customDropdown.js
@@ -1,101 +1,31 @@
-
 class CustomDropdown extends HTMLElement {
   constructor() {
     super();
   }
 
   connectedCallback() {
+    // Create a button for the dropdown
+    const dropButt = document.createElement('button');
+    dropButt.textContent = this.title; // Use the title attribute as the button text
+    dropButt.className = 'dropdown-button'; // Add a class to the button
 
-        const dropButt = document.createElement('button');
-        dropButt.textContent = this.title;
-        dropButt.className = 'dropdown-button';
-
-
-        const dropList = document.createElement('div');
-        dropList.className = 'dropdown-content';
-
-        for (let child of this.children){
-            const btn = document.createElement('button');
-            btn.textContent = child.textContent;
-            dropList.appendChild(btn);
-        }
-
-        this.innerHTML = '';
-        this.appendChild(dropButt);
-        this.appendChild(dropList);
-
-
+    // Create a list to hold the dropdown items
+    const dropList = document.createElement('div');
+    dropList.className = 'dropdown-content';
+    // Loop through each child element of the custom dropdown element
+    for (let child of this.children){
+      // Create a button for each child element
+      const btn = document.createElement('button');
+      btn.textContent = child.textContent; // Use the child element's text as the button text
+      btn.id = "header-"+child.textContent+"-button"; // Add an ID to the button
+      dropList.appendChild(btn); // Add the button to the dropdown list
     }
+
+    this.innerHTML = ''; // Clear the contents of the custom dropdown element
+    this.appendChild(dropButt); // Add the dropdown button to the custom dropdown element
+    this.appendChild(dropList); // Add the dropdown list to the custom dropdown element
+  }
 }
+
+// Register the custom element
 customElements.define('custom-dropdown', CustomDropdown);
-
-
-
-
-
-
-
-class CustomHeader extends HTMLElement {
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
-
-      const buttonsContainer = document.createElement('div');
-      buttonsContainer.classList.add('custom-header-buttons');
-
-      // Create the buttons and their dropdown menus
-      const buttonNames = [
-        {
-          name: "File",
-          subMenus: ["New", "Open", "Save", "Save As"],
-        },
-        {
-          name: "Edit",
-          subMenus: ["Cut", "Copy", "Paste"],
-        },
-        {
-          name: "View",
-          subMenus: ["Zoom In", "Zoom Out", "Full Screen"],
-        },
-      ];
-
-      buttonNames.forEach((buttonData) => {
-        const button = document.createElement('button');
-        button.textContent = buttonData.name;
-        buttonsContainer.appendChild(button);
-
-        const subMenu = document.createElement('div');
-        subMenu.classList.add('custom-header-submenu');
-        buttonData.subMenus.forEach((subMenuName) => {
-          const subMenuButton = document.createElement('button');
-          subMenuButton.textContent = subMenuName;
-          subMenuButton.id = "header-"+subMenuName+"-button";
-          subMenu.appendChild(subMenuButton);
-          subMenuButton.addEventListener('click', () => console.log('Button clicked asdsadasd'));
-        });
-
-        button.addEventListener('focusin', () => {
-        var width =( button.offsetLeft) ;
-        subMenu.style.left = width+'px'
-        subMenu.classList.toggle('show', true);
-        });
-       button.addEventListener('focusout', () => {
-          subMenu.classList.toggle('show', false);
-        });
-
-        buttonsContainer.appendChild(subMenu);
-      });
-
-    const textContainer = document.createElement('div');
-    textContainer.textContent = this.textContent;
-    textContainer.classList.add('custom-header-text');
-
-    this.innerHTML = '';
-    this.appendChild(buttonsContainer);
-    this.appendChild(textContainer);
-  }
-}
-
-customElements.define('custom-header', CustomHeader);
