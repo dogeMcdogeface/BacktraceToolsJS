148f08c13287d09638deba370d51dcd0363c311b
dogeMcdogeface <username@domain.example>
2023-02-19 18:49:16 +0100

    Implemented code to display solutions as tables in editor3

--- www/editor3/script.js
+++ www/editor3/script.js
@@ -4,15 +4,13 @@ const queryArea = document.getElementById("queryArea");
 const consoleArea = document.querySelector("console-area");
 const traceArea = document.getElementById("traceArea");
 
-// Main function that initializes the tool and calls testConsole()
+// Main function that initializes the page
 function main() {
    console.log("Starting Backtrace Tools");
-   //testConsole();
 }
 
 // Sends a query request to the API and displays the response in the console area
-function answerRequest() {
-   consoleArea.print("Executing Query ", "", "green");
+function requestAnswer() {
    const count = parseInt(document.getElementById("answer-number-input").value, 10);
    const query = {
       program: codeArea.value,
@@ -20,10 +18,13 @@ function answerRequest() {
       count: count,
       id: uniqueID(),
    };
-   console.log("Requesting", query);
-   //consoleArea.print('Requesting', query);
+   postQuery(query, displayResponse);
+}
+
+function postQuery(query, callback) {
+   consoleArea.print("Executing Query ", "", "green");
    const xhttp = new XMLHttpRequest();
-   xhttp.onload = () => displayResponse(xhttp.responseText);
+   xhttp.onload = () => callback(xhttp.responseText);
    xhttp.open("POST", "/api/query");
    xhttp.send(JSON.stringify(query));
 }
@@ -34,58 +35,53 @@ function displayResponse(responseText) {
    const solutions = responseObj.solutions;
    console.log("Receiving", responseObj);
    consoleArea.print("Query: ", responseObj.query, "Black");
-   if (solutions.length === 0) {
-      consoleArea.print("FALSE", "Red");
-   } else if (solutions.length === 1 && Object.keys(solutions[0]).length === 0) {
-      consoleArea.print("TRUE", "DarkGreen ");
-   } else {
-      consoleArea.print(solutions, "blue");
-             const table = buildTable(solutions);
-            consoleArea.insert(table);
-   }
+
+   consoleArea.print(solutions, "blue");
+   const table = buildAnswerTable(solutions);
+   consoleArea.insert(table);
 }
 
-function buildTable(data) {
-  const table = document.createElement('table');
-  const thead = document.createElement('thead');
-  const tbody = document.createElement('tbody');
+function buildAnswerTable(data) {
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
+  const tbody = document.createElement("tbody");
   table.classList.add("solutionTable");
-
-  // Create the header row with property names as columns
-  const headerRow = document.createElement('tr');
-  const numberHeader = document.createElement('th');
+  const headerRow = document.createElement("tr");
+  const numberHeader = document.createElement("th");
   numberHeader.textContent = "#";
   headerRow.appendChild(numberHeader);
-  for (const property in data[0]) {
-    const th = document.createElement('th');
-    th.textContent = property;
+
+  if (data.length === 0 || Object.keys(data[0]).length === 0) {
+    const th = document.createElement("th");
+    th.textContent = "Has Solutions";
     headerRow.appendChild(th);
+    data = data.length === 0 ? [{_: false}] : data.map(obj => Object.keys(obj).length === 0 ? { _: true, ...obj } : obj);
+  } else {
+    for (const property in data[0]) {
+      const th = document.createElement("th");
+      th.textContent = property;
+      headerRow.appendChild(th);
+    }
   }
   thead.appendChild(headerRow);
   table.appendChild(thead);
 
-  // Create a row for each object in the data array
   data.forEach((obj, index) => {
-    const row = document.createElement('tr');
-    const numberCell = document.createElement('td');
+    const row = document.createElement("tr");
+    const numberCell = document.createElement("td");
     numberCell.textContent = index + 1;
     row.appendChild(numberCell);
     for (const property in obj) {
-      const td = document.createElement('td');
-      if (Object.keys(obj).length === 0) {
-        td.textContent = "TRUE";
-      } else {
-        td.textContent = obj[property];
-      }
+      const td = document.createElement("td");
+      td.textContent = obj[property];
       row.appendChild(td);
     }
     tbody.appendChild(row);
   });
-  table.appendChild(tbody);
 
+  table.appendChild(tbody);
   return table;
 }
-
 
 
 // Clears the console area
@@ -121,7 +117,7 @@ function unknownButton() {
 
 // Object containing button handlers for each button on the page
 const buttonHandlers = {
-   "answer-request-button": answerRequest,
+   "answer-request-button": requestAnswer,
    "clear-console-button": clearConsole,
    "show-tree-button": showTree,
    "header-New-button": headerNew,
@@ -136,9 +132,9 @@ for (const button of document.querySelectorAll("button")) {
 
 queryArea.textarea.addEventListener("keydown", (event) => {
    if (event.key === "Enter") {
-      if (!event.ctrlKey ) {
+      if (!event.ctrlKey) {
          event.preventDefault();
-         answerRequest();
+         requestAnswer();
       } else if (event.ctrlKey) {
          event.preventDefault();
          queryArea.addNewLine();
@@ -150,19 +146,3 @@ queryArea.textarea.addEventListener("keydown", (event) => {
 const uniqueID = () => Math.random().toString(36).substr(2, 12);
 
 main();
-
-// Function that generates and displays random error messages in the console, as an example
-function testConsole() {
-   const htmlColors = ["red", "blue", "green", "orange", "purple", "pink", "brown", "gray", "black"];
-   const errorMessages = ["Fatal Exception", "Undefined Variable", "Syntax Error", "Index Out of Bounds", "Memory Overflow"];
-   for (let i = 0; i < 15; i++) {
-      const color = htmlColors[Math.floor(Math.random() * htmlColors.length)];
-      const message = `${errorMessages[Math.floor(Math.random() * errorMessages.length)]}: ${Math.random().toString(36).substring(2, 15)}`;
-      consoleArea.write(message, color);
-   }
-   const t = document.createElement`table`,
-      s = t.style;
-   s.border = "1px solid black";
-   for (let i = 2; i--; ) for (let r = t.insertRow(), j = 3; j--; ) (r.insertCell().style.cssText = "border:1px solid black;padding:10px"), (r.cells[2 - j].textContent = `Row ${i + 1}, Column ${j + 1}`);
-   consoleArea.insert(t);
-}
