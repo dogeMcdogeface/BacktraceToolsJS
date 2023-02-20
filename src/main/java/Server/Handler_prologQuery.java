2b2663a759bf5f77b8cc00ea0aead163d907fc87
dogeMcdogeface <username@domain.example>
2023-02-21 00:36:15 +0100

    Migrated literal string constants and debug prints to .properties files

--- src/main/java/Server/Handler_prologQuery.java
+++ src/main/java/Server/Handler_prologQuery.java
@@ -10,38 +10,33 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
 import java.util.Map;
 import java.util.ResourceBundle;
 
 //------    Handle AJAX call, containing PROLOG program and query   ------------------------------------//
 public class Handler_prologQuery implements HttpHandler {
     private static final ResourceBundle MESSAGES = ResourceBundle.getBundle("Messages");
-    private static final ResourceBundle SERVERBUNDLE = ResourceBundle.getBundle("Server");
+    private static final ResourceBundle SERVER = ResourceBundle.getBundle("Server");
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        System.out.println("Received Query");
+        System.out.println(MESSAGES.getString("query.received"));
         try {
             // Parse the request body as JSON
             String requestBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
             JsonNode requestJson = new ObjectMapper().readTree(requestBody);
 
             // Extract required values from the request
-            String program = requestJson.get("program").asText();
-            String query = requestJson.get("query").asText();
-            int count = requestJson.get("count").asInt();
-            String id = requestJson.get("id").asText();
+            String program = requestJson.get(SERVER.getString("request.program")).asText();
+            String query = requestJson.get(SERVER.getString("request.query")).asText();
+            int count = requestJson.get(SERVER.getString("request.count")).asInt();
+            String id = requestJson.get(SERVER.getString("request.id")).asText();
 
             // Check for missing or invalid values
             if (program == null || query.isEmpty() || count <= 0 || id.isEmpty()) {
                 throw new IllegalArgumentException("Request has missing or invalid fields");
             }
-
-            // Do something with the extracted values
-            System.out.println("program: " + program);
-            System.out.println("query: " + query);
-            System.out.println("count: " + count);
-            System.out.println("id: " + id);
 
             // Create a Map with the response data
             Map<String, Object> responseData = JPLInterface.execute(id, program, query, count);
@@ -52,18 +47,18 @@ public class Handler_prologQuery implements HttpHandler {
             exchange.sendResponseHeaders(200, response.length());
             exchange.getResponseBody().write(response.getBytes(StandardCharsets.UTF_8));
             exchange.close();
-            System.out.println("Query successful");
+            System.out.println(MESSAGES.getString("query.successful"));
         } catch (JsonProcessingException e) {
-            System.out.println("Query failed, malformed json");
+            System.out.println(MESSAGES.getString("query.failed.bad.json"));
             exchange.sendResponseHeaders(400, -1); // malformed json
         } catch  (IllegalArgumentException | NullPointerException e) {
-            System.out.println("Query failed, missing or invalid fields");
+            System.out.println(MESSAGES.getString("query.failed.bad.fields"));
             exchange.sendResponseHeaders(406, -1); // missing or invalid fields
         } catch (IOException e) {
-            System.out.println("Query failed, internal server error");
+            System.out.println(MESSAGES.getString("query.failed.server.error"));
             exchange.sendResponseHeaders(500, -1); // internal server error
         } catch (Exception e) {
-            System.err.println("Query failed, handled error: "+ e);
+            System.err.println(MessageFormat.format(MESSAGES.getString("query.failed.error"), e));
             exchange.sendResponseHeaders(536, -1); // internal server error
         } finally {
             exchange.close();
