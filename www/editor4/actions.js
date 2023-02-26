7a6196837ef9aedfef5d79971467bc52989a53ae
dogeMcdogeface <username@domain.example>
2023-02-26 12:35:20 +0100

    Improved page layout

--- www/editor4/actions.js
+++ www/editor4/actions.js
@@ -43,50 +43,45 @@ function buildAnswerTable(data) {
    return table;
 }
 
+setInterval(updateGUI, 500);
+function updateGUI() {
+   if (canExecQuery()) {
+      document.getElementById("answer-show-button").disabled = false;
+   } else {
+      document.getElementById("answer-show-button").disabled = true;
+   }
 
-setInterval(updateGUI, 1000);
-function updateGUI(){
-
-    if(canExecQuery()){
-        document.getElementById("answer-show-button").disabled = false;
-    }else{
-        document.getElementById("answer-show-button").disabled = true;
-    }
-
-    if(currentQuery.open){
-    document.getElementById("stop-query-button").disabled = false;
-    }else{
-    document.getElementById("stop-query-button").disabled = true;
-    }
-    document.getElementById("query-status-label").innerText = "Query status: " +currentQuery.state;
-
+   if (currentQuery.open) {
+      document.getElementById("stop-query-button").disabled = false;
+   } else {
+      document.getElementById("stop-query-button").disabled = true;
+   }
+   document.getElementById("query-status-label").innerText = "Query status: " + currentQuery.state;
 }
 
-
-function clearTrace(){
-traceText.innerHTML="";
-
+function clearTrace() {
+   traceText.innerHTML = "";
 }
 
-function printToTrace(txt){
-        const div = document.createElement("div");
-        const textNode = document.createTextNode(txt);
-        div.appendChild(textNode);
-        traceText.appendChild(div);
+function printToTrace(txt) {
+   const div = document.createElement("div");
+   const textNode = document.createTextNode(txt);
+   div.appendChild(textNode);
+   traceText.appendChild(div);
 }
 
 function displayTrace(trace) {
-clearTrace();
-    trace.forEach((element, index) => {
-        const div = document.createElement("div");
-        const textNode = document.createTextNode(element);
-        div.appendChild(textNode);
-        div.style.display = "none"; // Set the div element to be hidden
-        traceText.appendChild(div);
-        //const delay = 50 * Math.min(10, index);
-        const delay = 500 /(1+ (1/(index*0.5)));
-        setTimeout(() => {
-            div.style.display = "block"; // Set the div element to be visible after 50ms
-        }, delay); // Multiply the delay by the index to stagger the display of each element
-    });
+   clearTrace();
+   trace.forEach((element, index) => {
+      const div = document.createElement("div");
+      const textNode = document.createTextNode(element);
+      div.appendChild(textNode);
+      div.style.display = "none"; // Set the div element to be hidden
+      traceText.appendChild(div);
+      //const delay = 50 * Math.min(10, index);
+      const delay = 500 / (1 + 1 / (index * 0.5));
+      setTimeout(() => {
+         div.style.display = "block"; // Set the div element to be visible after 50ms
+      }, delay); // Multiply the delay by the index to stagger the display of each element
+   });
 }
