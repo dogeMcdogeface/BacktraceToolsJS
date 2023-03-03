ad1e5e3c4532cababd37c84a88f50caa2ed005bd
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2023-03-03 04:22:28 +0100

    Implemented sticky auto scrolling

--- www/htmlComponents/CC/console-area.js
+++ www/htmlComponents/CC/console-area.js
@@ -10,8 +10,8 @@ class ConsoleArea extends HTMLElement {
          const line = document.createElement("div");
          line.textContent = text;
          line.style.color = color;
-         this.appendChild(line);
-         this.scrollToBottom();
+         this.insert(line);
+         //this.scrollToBottom();
       };
 
       this.print = function (...args) {
@@ -21,28 +21,49 @@ class ConsoleArea extends HTMLElement {
          const div = document.createElement("div");
          div.style.color = color;
          div.textContent = message;
-         this.appendChild(div);
+         this.insert(div);
          this.scrollToBottom();
       };
 
+      const bottomer = document.createElement("div");
+
       this.insert = function (element) {
          this.appendChild(element);
-         this.scrollToBottom();
+         this.appendChild(bottomer);
+
+         //this.scrollToBottom();
       };
 
       this.scrollToBottom = function () {
          this.scrollTo(0, this.scrollHeight);
       };
 
-      const observer = new MutationObserver((mutationsList, observer) => {
-         for (const mutation of mutationsList) {
-            if (mutation.type === "childList") {
-               this.scrollToBottom();
-            }
-         }
-      });
 
+
+let isAtBottom = true;
+this.addEventListener('scroll', () => {
+  isAtBottom = this.isInViewport(bottomer);
+  //console.log(isAtBottom);
+});
+
+
+const observer = new MutationObserver((mutationsList) => {
+  if (mutationsList.some((mutation) => mutation.type === 'childList' && isAtBottom)) {
+    this.scrollToBottom();
+  }
+});
       observer.observe(this, { childList: true, subtree: true });
+   }
+
+
+   isInViewport(element) {
+       const rect = element.getBoundingClientRect();
+       return (
+           rect.top >= 0 &&
+           rect.left >= 0 &&
+           rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+           rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+       );
    }
 }
 
