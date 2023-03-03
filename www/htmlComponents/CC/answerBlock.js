4e4984be8651784bfdec46a9b5e818239fdd111f
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2023-03-03 04:22:58 +0100

    improved solution displaying for multithreaded prolog engines

--- www/htmlComponents/CC/answerBlock.js
+++ www/htmlComponents/CC/answerBlock.js
@@ -3,61 +3,61 @@ class AnswerBlock extends HTMLElement {
       super();
    }
 
-connectedCallback() {
-  // Create the header bar with label and buttons
-  this.setAttribute('tabindex', '0');
+   connectedCallback() {
+      // Create the header bar with label and buttons
+      this.setAttribute("tabindex", "0");
 
-  const header = document.createElement("div");
-  header.classList.add("header");
+      const header = document.createElement("div");
+      header.classList.add("header");
 
-  this.labelElement = document.createElement("span");
-  this.labelElement.textContent = this.getAttribute("title") || "New Query";
-  header.appendChild(this.labelElement);
+      this.labelElement = document.createElement("span");
+      this.labelElement.textContent = this.getAttribute("title") || "New Query";
+      header.appendChild(this.labelElement);
 
-  const addButton = document.createElement("button");
-  addButton.textContent = "Stop";
-  addButton.addEventListener("click", this.addRow.bind(this));
-  header.appendChild(addButton);
+      const addButton = document.createElement("button");
+      addButton.textContent = "Stop";
+      addButton.addEventListener("click", this.addRow.bind(this));
+      header.appendChild(addButton);
 
-  const hideButton = document.createElement("button");
-  hideButton.id = "hideButton";
-  hideButton.textContent = "⌵";
-  hideButton.addEventListener("click", this.toggleTable.bind(this));
-  header.appendChild(hideButton);
+      const hideButton = document.createElement("button");
+      hideButton.id = "hideButton";
+      hideButton.textContent = "⌵";
+      hideButton.addEventListener("click", this.toggleTable.bind(this));
+      header.appendChild(hideButton);
 
-  this.appendChild(header);
+      this.appendChild(header);
 
-  // Create the table body
-  const tableDiv = document.createElement("div");
-  tableDiv.classList.add("body");
+      // Create the table body
+      const tableDiv = document.createElement("div");
+      tableDiv.classList.add("body");
 
-  const table = document.createElement("table");
-  const thead = document.createElement("thead");
-  const tbody = document.createElement("tbody");
-  table.appendChild(thead);
-  table.appendChild(tbody);
-  tableDiv.appendChild(table);
+      const table = document.createElement("table");
+      const thead = document.createElement("thead");
+      const tbody = document.createElement("tbody");
+      table.appendChild(thead);
+      table.appendChild(tbody);
+      tableDiv.appendChild(table);
 
-  // Save the table body for later use
-  this.thead = thead;
-  this.tbody = tbody;
+      // Save the table body for later use
+      this.body = tableDiv;
+      this.thead = thead;
+      this.tbody = tbody;
 
-  // Add error label at the bottom of the table body
-  const errorLabel = document.createElement("div");
-  errorLabel.classList.add("errorlabel");
-  tableDiv.appendChild(errorLabel);
+      // Add error label at the bottom of the table body
+      const errorLabel = document.createElement("div");
+      errorLabel.classList.add("errorlabel");
+      tableDiv.appendChild(errorLabel);
 
-  this.appendChild(tableDiv);
+      this.appendChild(tableDiv);
 
-  // Save the error label for later use
-  this.errorLabel = errorLabel;
-}
+      // Save the error label for later use
+      this.errorLabel = errorLabel;
+   }
 
-// Function to set the value of the error label
-setError(value) {
-  this.errorLabel.textContent = value;
-}
-
+   // Function to set the value of the error label
+   setError(value) {
+      this.errorLabel.textContent = value;
+   }
 
    set title(value) {
       this.labelElement.textContent = value;
@@ -67,9 +67,12 @@ setError(value) {
       const keys = Object.keys(data);
       if (this.thead.innerHTML === "") {
          const headerRow = document.createElement("tr");
-         headerRow.insertCell().textContent = "#";
+         headerRow.insertCell().outerHTML = "<th>#</th>";
          for (const key of keys) {
-            headerRow.insertCell().textContent = key;
+            const th = document.createElement("th");
+            th.textContent = key;
+            headerRow.appendChild(th);
+            // headerRow.insertCell().textContent = key;
          }
          this.thead.appendChild(headerRow);
       }
@@ -80,6 +83,7 @@ setError(value) {
          row.insertCell().textContent = txt;
       }
       this.tbody.appendChild(row);
+      this.body.style.maxHeight = (this.body.scrollHeight + 100)+"px";
    }
 
    toggleTable() {
